d64cc9d23952300e2c3f13acfa6c93fa1b5e986f
Szymon Tyburek <szymontyburek10@gmail.com>
2024-09-14 13:53:20 -0700

    refactored: declared input and output variables for corresponding pins

--- Button/button/button.ino
+++ Button/button/button.ino
@@ -1,13 +1,16 @@
+int output = 12;
+int input = 2;
+
 void setup() {
-    pinMode(12, OUTPUT);
-    pinMode(2, INPUT);
+    pinMode(output, OUTPUT);
+    pinMode(input, INPUT);
 }
 
 void loop() {
-  if(digitalRead(2) == HIGH) { //read voltage of pin 2
-    digitalWrite(12, HIGH);
+  if(digitalRead(input) == HIGH) { //read voltage of pin 2
+    digitalWrite(output, HIGH);
   }
   else {
-    digitalWrite(12, LOW);
+    digitalWrite(output, LOW);
   }
 }
